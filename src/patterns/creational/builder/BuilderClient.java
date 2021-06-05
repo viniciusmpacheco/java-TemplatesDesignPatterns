d8cdff465fe57c3aba3d17468620c930c69821cc
Vinicius Pacheco <vinicius.mpacheco@hotmail.com>
2021-06-05 18:18:22 -0300

    Builder pattern implemented

---
+++ src/patterns/creational/builder/BuilderClient.java
@@ -0,0 +1,24 @@
+package patterns.creational.builder;
+
+import patterns.creational.builder.model.FastFoodMeal;
+
+public class BuilderClient {
+	public static void main(String[] args) {
+		FastFoodMeal burguerCombo = new FastFoodMeal.Builder("Fries")
+				.andMain("CheeseBurger")
+				.forDrink("Code")
+				.thatsAll();
+		System.out.println(burguerCombo);
+		
+		FastFoodMeal justFries = new FastFoodMeal.Builder("Fries").thatsAll();
+		System.out.println(justFries);
+		
+		FastFoodMeal heartAtackCombo = new FastFoodMeal.Builder("Large Fries")
+				.andMain("Monster Burguer")
+				.forDrink("Milk Shake")
+				.andDessert("Fudge Cake")
+				.andGift("2 Kilograms")
+				.thatsAll();
+		System.out.println(heartAtackCombo);
+	}
+}
