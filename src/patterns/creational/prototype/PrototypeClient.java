1f89e3dbc99bd91eff950670c54fc87a5e77d233
Vinicius Pacheco <vinicius.mpacheco@hotmail.com>
2021-06-05 18:14:23 -0300

    Prototype pattern implemented

---
+++ src/patterns/creational/prototype/PrototypeClient.java
@@ -0,0 +1,19 @@
+package patterns.creational.prototype;
+
+import patterns.creational.prototype.model.*;
+
+public class PrototypeClient {
+	public static void main(String[] args) throws CloneNotSupportedException {
+		User user = new User("ToClone", 25, new Address("ABC Street", 1000));
+		System.out.println(user);
+		
+		User cloneUser = user.cloneBuilder().withName("Cloned++").now();
+		System.out.println(cloneUser);
+		
+		User anotherUser = cloneUser.cloneBuilder()
+				.withAge(100)
+				.withAddress(new Address("A Street", 500))
+				.now();
+		System.out.println(anotherUser);
+	}
+}
